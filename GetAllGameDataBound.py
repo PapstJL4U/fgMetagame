88e5193fa36b7a2c85e42650d40461f2821551d8
PapstJL4U <PapstJL4U@users.noreply.github.com>
2021-03-02 00:47:06 +0100

    added SF5 support
    ordered the files and added subfolders
    added all of SF5 official matchupcharts
    clean up some files

--- GetAllGameDataBound.py
+++ GetAllGameDataBound.py
@@ -2,21 +2,31 @@
 import os
 import Fighting_games_bounds as fgb
 
-def filelist():
+def streetfighter():
     """create a list of csv files with matchupdata"""
     list = []
     print('Using the following files:')
-    for file in os.listdir():
+    for file in os.listdir("Street Fighter 5"):
         if file.lower().endswith('.csv') and "streetfighter" in file.lower():
             list.append(file)
             print(file)
     return list
 
+def guiltygear():
+    """create a list of csv files with matchupdata"""
+    list = []
+    print('Using the following files:')
+    for file in os.listdir("Guilty Gear"):
+        if file.lower().endswith('.csv'):
+            list.append(file)
+            print(file)
+    return list
+
 def main():
-    csv_list = filelist()
+    csv_list = streetfighter()
     for csv in csv_list:
         print(csv)
-        fgb.main_para(csv)
+        fgb.main_para("Street Fighter 5"+os.sep+csv)
         print('Finished: '+csv)
 
     print('Finished with everything.')
---
+++ Street Fighter 5/sfvmatchupdata _2021.py
@@ -0,0 +1,60 @@
+"""Loads all matchup data of streetfighter from the offical website and puts them into .csv files"""
+from bs4 import BeautifulSoup
+import urllib.request, sfchars
+
+year1 = (201610, 201611, 201612)
+year2 = (201701, 201702, 201703, 201704, 201705, 201706, 201707, 201708, 201709, 201710, 201711, 201712)
+year3 = (201801, 201802, 201803, 201804, 201805, 201806, 201807, 201808, 201809, 201810, 201811, 201812)
+year4 = (201901, 201902, 201903, 201904, 201905, 201906, 201907, 201908, 201909, 201910, 201911, 201912)
+year5 = (202001, 202002, 202003, 202004, 202005, 202006, 202007, 202008, 202009, 202010, 202011, 202012)
+year6 = (202101,)# 202102, 202103, 202104, 202105, 202106, 202107, 202108, 202109, 202110, 202111, 202112)
+total = year1 + year2 + year3 + year4 + year5 + year6
+streetfighter5="https://game.capcom.com/cfn/sfv/stats/dia/"
+
+sf5_month = year4 + year5 +year6
+for month in total:
+    #get url and create readable html to extract matchup data
+    url = urllib.request.urlopen(streetfighter5+str(month))
+    soup = BeautifulSoup(url, "html.parser")
+    data = soup.table
+    
+    #find html table with data
+    table = soup.find("table", {"id": "tbl"})
+    tbody = soup.find("tbody")
+    all_chars = tbody.findAll("tr")
+
+    with open('StreetFighter5-'+str(month)+'.csv','w', encoding="utf-8") as f:
+        
+        for i,char in enumerate(all_chars):
+
+            row = char.findAll("td")
+            output = ""        
+
+            for j, cell in enumerate(row):
+                #print(cell.get_text())
+
+                #Find the name of the character (hidden in the image path name as 1-3 letters)
+                if cell.find("img") != None:
+                    name = cell.find("img")["src"]
+                    eindex = name.index(".png")
+                    bindex = len(name) - name[::-1].index("/")
+                    name = sfchars.translation_dic[name[bindex:eindex]]
+                    #print(name+" ", end='')
+                    output+=name
+
+                #mirror matchups are shown as "-" and not as 0.5
+                elif cell.get_text() == "-":
+                    #print(str(5.000)+" ", end='')
+                    output+=",0.500"
+
+                #change X.yz to 0.xyz
+                else:
+                    #print(cell.get_text()+" ", end='')
+                    output+=","+str(round(0.1*float(cell.get_text()),3))
+
+            #last entry avoids empty line
+            if i+1==len(all_chars):
+                f.write('{}'.format(output))               
+            else:
+                f.write('{}\n'.format(output))
+    print("Done: "+str(month))
